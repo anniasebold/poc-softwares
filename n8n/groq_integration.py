75111c1b5a7424ce4f60478909506e9635fbb433
anniasebold <anniasebold3.0@gmail.com>
2025-04-21 20:29:59 -0400

    feat: enhance question_request function and add response handling with CSV storage

--- n8n/groq_integration.py
+++ n8n/groq_integration.py
@@ -1,9 +1,15 @@
 import os
 from groq import Groq
 
-def question_request(city: str, state: str):
+PROMPT = """
+Você é um assistente de turismo.
+Me fale lugares turísticos em {city} - {state}.
+Fale de forma clara e objetiva, com no máximo 5 frases.
+Fale em forma de lista com itens numerados.
+"""
+
+def question_request(city: str, state: str) -> str | None:
     api_key = os.getenv("GROQ_API_KEY")
-    print("GROQ API Key:", api_key)
     if not api_key:
         print("Please set the GROQ_API_KEY environment variable.")
         return
@@ -16,10 +22,10 @@ def question_request(city: str, state: str):
         messages=[
             {
                 "role": "user",
-                "content": f"Me fale lugares turísticos em {city} - {state}.",
+                "content": PROMPT.format(city=city, state=state),
             }
         ],
         model="llama-3.3-70b-versatile",
     )
 
-    print(chat_completion.choices[0].message.content)
+    return chat_completion.choices[0].message.content
--- n8n/test.py
+++ n8n/test.py
@@ -1,40 +1,69 @@
+import csv
 import json
+import markdown
+import os
 import requests
 from groq_integration import question_request
 
 def init(): 
     name = input("Digite seu nome (máximo 20 caracteres): ")
-    age = input("Digite sua idade (mínimo 18 anos): ") 
+    age = input("Digite sua idade: ")
     city = input("Digite a sua cidade: ")
     state = input("Digite o seu estado (em formato de sigla): ")
+    approved = True
 
     if not name or not age or not city or not state:
         print("Nome, idade, cidade e estado são obrigatórios.")
         return
-    
+
     if len(name) > 20:
         print("Nome deve ter no máximo 20 caracteres.")
         return
-    
+
     if len(state) > 2:
         print("Estado deve ter no máximo 2 caracteres.")
-        return
-
-    if not age.isdigit() or int(age) < 18:
-        print("Idade deve ser um número e maior ou igual a 18.")
         return
 
     if len(city) > 50:
         print("Cidade deve ter no máximo 50 caracteres.")
         return
 
-    question_request(city, state)
+    if not age.isdigit() or int(age) < 18:
+        approved = False
 
-def send_request(name: str, age: str):
+    answer_response = question_request(city, state)
+    response_formatted = markdown.markdown(answer_response).replace('\n', '<br>')
+    send_request(name, age, approved, response_formatted)
+    save_response(name, age, city, state, approved, answer_response)
+
+def save_response(name: str, age: str, city: str, state: str, approved: bool, answer_response: str):
+    if not answer_response:
+        print("Houve um erro ao obter a resposta.")
+        return
+
+    file_path = "respostas.csv"
+    file_exists = os.path.isfile(file_path)
+    is_empty = not file_exists or os.path.getsize(file_path) == 0
+
+    try:
+        with open(file_path, mode="a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+
+            if is_empty:
+                writer.writerow(["name", "age", "city", "state", "approved", "answer_response"])
+
+            writer.writerow([name, age, city, state, approved, answer_response])
+        print("Dados salvos com sucesso em respostas.csv")
+    except Exception as e:
+        print(f"Erro ao salvar os dados no CSV: {e}")
+
+def send_request(name: str, age: str, approved: bool, answer_response: str):
     url = "http://localhost:5678/webhook-test/send-email"
     params = {
         "name": name,
-        "age": age
+        "age": age,
+        "approved": approved,
+        "response": answer_response
     }
 
     try:
